f8db8af450bff2abdd4c29a9d2babb76434199d4
thengelsec <ssuk83158315@gmail.com>
2025-10-08 20:04:12 +0900

    Fix: Implement selectedDayPredicate for MainCalendar

--- lib/week05/calendar_scheduler/component/main_calendar.dart
+++ lib/week05/calendar_scheduler/component/main_calendar.dart
@@ -10,10 +10,16 @@ class MainCalendar extends StatelessWidget {
     required this.onDaySelected,
     required this.selectedDate,
   });
-  
+
   @override
   Widget build(BuildContext context) {
     return TableCalendar(
+      onDaySelected: onDaySelected,
+      // 날짜 선택 시 실행할 함수
+      selectedDayPredicate: (date) => // 선택된 날짜를 구분할 로직
+        date.year == selectedDate.year &&
+        date.month == selectedDate.month &&
+        date.day == selectedDate.day,
       firstDay: DateTime(1800, 1, 1),   // 첫째 날
       lastDay: DateTime(3000, 1, 1),    // 마지막 날
       focusedDay: DateTime.now(),       // 화면에 보여지는 날
