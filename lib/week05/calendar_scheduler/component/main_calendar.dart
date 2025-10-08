b62bcb08c37f335bdb77838f9a82ef51e81aed70
thengelsec <ssuk83158315@gmail.com>
2025-10-08 19:49:12 +0900

    feat: Implement basic MainCalendar widget with TableCalendar

---
+++ lib/week05/calendar_scheduler/component/main_calendar.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class MainCalendar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar(
+      firstDay: DateTime(1800, 1, 1),   // 첫째 날
+      
+    )
+  }
+}
