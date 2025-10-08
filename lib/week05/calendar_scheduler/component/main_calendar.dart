01b077698e522f46bc51cbb96aee9efd0366d007
thengelsec <ssuk83158315@gmail.com>
2025-10-08 19:57:12 +0900

    feat(calendar): Style calendar header and default day cells

--- lib/week05/calendar_scheduler/component/main_calendar.dart
+++ lib/week05/calendar_scheduler/component/main_calendar.dart
@@ -9,6 +9,25 @@ class MainCalendar extends StatelessWidget {
       firstDay: DateTime(1800, 1, 1),   // 첫째 날
       lastDay: DateTime(3000, 1, 1),    // 마지막 날
       focusedDay: DateTime.now(),       // 화면에 보여지는 날
+      headerStyle: HeaderStyle(       // 달력 최상단 스타일
+        titleCentered: true,          // 제목 중앙에 위치하기
+        formatButtonVisible: false,   // 달력 크기 선택 옵션 없애기
+        titleTextStyle: TextStyle(    // 제목 글꼴
+          fontWeight: FontWeight.w700,
+          fontSize: 16.0,
+        ),
+      ),
+      calendarStyle: CalendarStyle(
+        isTodayHighlighted: false,
+        defaultDecoration: BoxDecoration( // 기본 날짜 스타일
+          borderRadius: BorderRadius.circular(6.0),
+          color: LIGHT_GREY_COLOR,
+        ),
+        weekendDecoration: BoxDecoration( // 주말 날짜 스타일
+        borderRadius: BorderRadius.circular(6.0),
+        c
+        )
+      )
     );
   }
 }
