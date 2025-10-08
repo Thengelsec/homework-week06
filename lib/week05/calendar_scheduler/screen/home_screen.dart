e83400ac6989bac4e62db51904910255ffd4fd29
thengelsec <ssuk83158315@gmail.com>
2025-10-08 21:22:44 +0900

    Fix: Correct typo in import statement in HomeScreen

--- lib/week05/calendar_scheduler/screen/home_screen.dart
+++ lib/week05/calendar_scheduler/screen/home_screen.dart
@@ -2,7 +2,7 @@ import 'package:myapp/week05/calendar_scheduler/component/main_calendar.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/week05/calendar_scheduler/component/schedule_card.dart';
 import 'package:myapp/week05/calendar_scheduler/component/today_banner.dart';
-import 'package:myapp/week05/calendar_scheduler/component/schedule_bottom_sheet.dart';
+import 'package:myapp/week05/calendar_scheduler/component/schedule_boi';
 
 
 class HomeScreen extends StatefulWidget {
