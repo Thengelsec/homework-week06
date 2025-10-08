7e84bf4626b708b8caf16f1934af52ab76e074e5
thengelsec <ssuk83158315@gmail.com>
2025-10-08 21:29:13 +0900

    feat: Implement CustomTextField component

---
+++ lib/week05/calendar_scheduler/component/custom_text_field.dart
@@ -0,0 +1,28 @@
+import 'package:myapp/week05/calendar_scheduler/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String label;
+
+  const CustomTextField({
+    required this.label,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(  // 세로로 텍스트와 텍스트 필드 배치
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: TextStyle(
+            color: PRIMARY_COLOR,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        TextFromField(),  // 폼 안에서 텍스트 필드를 쓸 때 사용
+      ],
+    );
+  }
+}
