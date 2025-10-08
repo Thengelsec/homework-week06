1a98bd847ae95656ed633347f16ca4f1609c9188
thengelsec <ssuk83158315@gmail.com>
2025-10-08 21:09:16 +0900

    feat: Implement ScheduleCard component

--- lib/week05/calendar_scheduler/component/schedule_card.dart
+++ lib/week05/calendar_scheduler/component/schedule_card.dart
@@ -1,6 +1,52 @@
 import 'package:myapp/week05/calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
 
+// _Time 위젯 위에 작성
+class ScheduleCard extends StatelessWidget {
+  final int startTime;
+  final int endTime;
+  final String content;
+
+  const ScheduleCard({
+    required this.startTime,
+    required this.endTime,
+    required this.content,
+    Key? key
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1.0,
+          color: PRIMARY_COLOR,
+        ),
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: IntrinsicHeight(   // 높이를 내부 위젯들의 최대 높이로 설정
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _Time(  // 시작과 종료 시간을 보여줄 위젯
+                startTime: startTime,
+                endTime: endTime,
+              ),
+              SizedBox(width: 16.0),
+              _Content(   // 일정 내용을 보여줄 위젯
+                content: content,
+              ),
+              SizedBox(width: 16.0),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class _Time extends StatelessWidget {
   final int startTime;  // 시작 시간
   final int endTime;    // 종료 시간
