325e3f1280bc6ef3795c4a1746c85adc93deec0e
thengelsec <ssuk83158315@gmail.com>
2025-10-08 19:46:12 +0900

    feat: Implement basic calendar scheduler app

---
+++ lib/week05/calendar_scheduler/main.dart
@@ -0,0 +1,10 @@
+import 'package:myapp/week05/calendar_scheduler/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
