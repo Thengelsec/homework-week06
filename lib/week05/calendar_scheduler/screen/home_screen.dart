f8cb8380ab606486866b387eb796e571b1982b96
thengelsec <ssuk83158315@gmail.com>
2025-10-08 20:07:12 +0900

    feat(calendar): Add stateful HomeScreen with selected date

--- lib/week05/calendar_scheduler/screen/home_screen.dart
+++ lib/week05/calendar_scheduler/screen/home_screen.dart
@@ -6,6 +6,17 @@ class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime.utc( // 선택된 날짜를 관리할 변수
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(   // 시스템 UI 피해서 UI 구현하기
