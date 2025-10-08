c75bcaa56214b3ca660831f0107678cc2ede4f64
thengelsec <ssuk83158315@gmail.com>
2025-10-08 21:23:44 +0900

    feat: Add schedule bottom sheet and colors constant

--- lib/week05/calendar_scheduler/screen/home_screen.dart
+++ lib/week05/calendar_scheduler/screen/home_screen.dart
@@ -2,7 +2,8 @@ import 'package:myapp/week05/calendar_scheduler/component/main_calendar.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/week05/calendar_scheduler/component/schedule_card.dart';
 import 'package:myapp/week05/calendar_scheduler/component/today_banner.dart';
-import 'package:myapp/week05/calendar_scheduler/component/schedule_boi';
+import 'package:myapp/week05/calendar_scheduler/component/schedule_bottom_sheet.dart';
+import 'package:myapp/week05/calendar_scheduler/const/colors.dart';
 
 
 class HomeScreen extends StatefulWidget {
