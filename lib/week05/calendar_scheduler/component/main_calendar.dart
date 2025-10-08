259a542eff098b778801af053e9e44f5047a3a8d
thengelsec <ssuk83158315@gmail.com>
2025-10-08 19:50:42 +0900

    feat(calendar): Initialize TableCalendar with date range and focused day.

--- lib/week05/calendar_scheduler/component/main_calendar.dart
+++ lib/week05/calendar_scheduler/component/main_calendar.dart
@@ -6,7 +6,8 @@ class MainCalendar extends StatelessWidget {
   Widget build(BuildContext context) {
     return TableCalendar(
       firstDay: DateTime(1800, 1, 1),   // 첫째 날
-      
-    )
+      lastDay: DateTime(3000, 1, 1),    // 마지막 날
+      focusedDay: DateTime.now(),       // 화면에 보여지는 날
+    );
   }
 }
