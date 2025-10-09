0cb93531db076679d005e54d5250288388913ee7
thengelsec <ssuk83158315@gmail.com>
2025-10-09 18:45:33 +0900

    feat: Add path and io imports to drift_database.dart

--- lib/week06/calendar_scheduler/database/drift_database.dart
+++ lib/week06/calendar_scheduler/database/drift_database.dart
@@ -3,6 +3,8 @@ import 'package:drift/drift.dart';
 
 import 'package:drift/native.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+import 'dart:io';
 
 // private값까지 불러올 수 있음
 part 'drift_database.g.dart';   // part 파일 지정
