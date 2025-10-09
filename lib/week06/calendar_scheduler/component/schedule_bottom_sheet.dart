a1f943e33924264d2a11eba8c1b2b0c210b11251
thengelsec <ssuk83158315@gmail.com>
2025-10-09 19:25:23 +0900

    feat(calendar_scheduler): Implement ScheduleBottomSheet component

---
+++ lib/week06/calendar_scheduler/component/schedule_bottom_sheet.dart
@@ -0,0 +1,26 @@
+import 'package:myapp/week06/calendar_scheduler/component/custom_text_field.dart';
+import 'package:myapp/week05/calendar_scheduler/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class ScheduleBottomSheet extends StatefulWidget {
+  const ScheduleBottomSheet({Key? key}) : super(key: key);
+
+  @override
+  State<ScheduleBottomSheet> createState() => _ScheduleBottomSheetState();
+}
+
+class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
+  @override
+  Widget build(BuildContext context) {
+    // 키보드 높이 가져오기
+    final bottomInset = MediaQuery.of(context).viewInsets.bottom;
+
+    return SafeArea(
+      child: Container(
+        // 화면에 반 높이에 키보드 높이 추가하기
+        height: MediaQuery.of(context).size.height / 2 + bottomInset, //
+        
+      )
+    )
+  }
+}
