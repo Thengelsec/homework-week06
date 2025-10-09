cd15114f3c78171684b7e4337077f9ef54caa4cc
thengelsec <ssuk83158315@gmail.com>
2025-10-09 20:06:43 +0900

    feat: Import GetIt and Drift database for DI

--- lib/week06/calendar_scheduler/screen/home_screen.dart
+++ lib/week06/calendar_scheduler/screen/home_screen.dart
@@ -4,6 +4,8 @@ import 'package:myapp/week05/calendar_scheduler/component/schedule_card.dart';
 import 'package:myapp/week05/calendar_scheduler/component/today_banner.dart';
 import 'package:myapp/week06/calendar_scheduler/component/schedule_bottom_sheet.dart';
 import 'package:myapp/week05/calendar_scheduler/const/colors.dart';
+import 'package:get_it/get_it.dart';
+import 'package:myapp/week06/calendar_scheduler/database/drift_database.dart';
 
 
 class HomeScreen extends StatefulWidget {
