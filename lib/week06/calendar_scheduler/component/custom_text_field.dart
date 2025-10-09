ab64db70fd7912788750edb9751800a3e4c2fb0c
thengelsec <ssuk83158315@gmail.com>
2025-10-09 19:14:52 +0900

    feat: Implement CustomTextField component

--- lib/week06/calendar_scheduler/component/custom_text_field.dart
+++ lib/week06/calendar_scheduler/component/custom_text_field.dart
@@ -2,3 +2,33 @@ import 'package:myapp/week05/calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+class CustomTextField extends StatelessWidget {
+  final String label; // 텍스트 필드 제목
+  final bool isTime;  // 시간 선택하는 텍스트 필드인지 여부
+
+  const CustomTextField({
+    required this.label,
+    required this.isTime,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(  // 세로로 텍스트와 텍스트 필드 배치
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: TextStyle(
+            color: PRIMARY_COLOR,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        Expanded(
+          flex: isTime ? 0 : 1, //
+          
+        )
+      ]
+    )
+  }
+}
