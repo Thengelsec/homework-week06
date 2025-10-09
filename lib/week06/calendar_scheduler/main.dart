20896175c94f6469c65fe301d3217d985915d727
thengelsec <ssuk83158315@gmail.com>
2025-10-09 18:51:33 +0900

    Fix: Initialize WidgetsFlutterBinding

--- lib/week06/calendar_scheduler/main.dart
+++ lib/week06/calendar_scheduler/main.dart
@@ -2,5 +2,5 @@ import 'package:myapp/week06/calendar_scheduler/database/drift_database.dart';
 import 'package:get_it/get_it.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensu
+  WidgetsFlutterBinding.ensureInitialized();
 }
