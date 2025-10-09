1ec58ab1059920e8332c4a1d967822615fbcd63a
thengelsec <ssuk83158315@gmail.com>
2025-10-09 18:47:03 +0900

    feat(drift_database): Add database schema version and code generation comment

--- lib/week06/calendar_scheduler/database/drift_database.dart
+++ lib/week06/calendar_scheduler/database/drift_database.dart
@@ -14,8 +14,6 @@ part 'drift_database.g.dart';   // part 파일 지정
     Schedules,
   ],
 )
-class LocalDatabase extends _$LocalDatabase {}
-// Code Generation으로 생성할 클래스 상속
 
 class LocalDatabase extends _$LocalDatabase {
   Stream<List<Schedule>> watchSchedules(DateTime data) =>
@@ -34,3 +32,4 @@ class LocalDatabase extends _$LocalDatabase {
   @override
   int get schemaVersion => 1;
 }
+// Code Generation으로 생성할 클래스 상속
